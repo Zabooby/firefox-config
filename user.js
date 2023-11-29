10ad9fa30a6d7a3bdd4a25fec85f750b2a46de44
Zabooby <78969986+Zabooby@users.noreply.github.com>
2023-11-29 19:38:13 +0000

    Update user.js

--- user.js
+++ user.js
@@ -1,13 +1,18 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
+### Settings for css customising ### 
+
+user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+### Privacy ### 
 
 user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.enabled", false); // see [NOTE]
+user_pref("toolkit.telemetry.enabled", false); 
 user_pref("toolkit.telemetry.server", "data:,");
 user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("toolkit.telemetry.newProfilePing.enabled", false); // [FF55+]
-user_pref("toolkit.telemetry.shutdownPingSender.enabled", false); // [FF55+]
-user_pref("toolkit.telemetry.updatePing.enabled", false); // [FF56+]
-user_pref("toolkit.telemetry.bhrPing.enabled", false); // [FF57+] Background Hang Reporter
-user_pref("toolkit.telemetry.firstShutdownPing.enabled", false); // [FF57+]
+user_pref("toolkit.telemetry.newProfilePing.enabled", false); 
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false); 
+user_pref("toolkit.telemetry.bhrPing.enabled", false); 
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false); 
 
