fdd159600ea745c37d4f4bb578de710169ed7933
Zabooby <78969986+Zabooby@users.noreply.github.com>
2025-02-08 10:46:14 +0000

    Update

--- user.js
+++ user.js
@@ -6,6 +6,8 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 /* Miscellaneous */
 
+user_pref("browser.uidensity", 1);
+user_pref("browser.uidensity", true);
 user_pref("ui.textHighlightBackground", "#213192");
 user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("ui.textSelectAttentionBackground", "#29bfba");
