38104f91009f50fa0c23d5fe71a01b235821d9cf
Zabooby <78969986+Zabooby@users.noreply.github.com>
2025-09-20 15:19:44 +0100

    Update

--- user.js
+++ user.js
@@ -5,6 +5,7 @@ user_pref("sidebar.revamp", false);
 user_pref("widget.windows.mica.popups", 0);
 user_pref("browser.compactmode.show", true);
 user_pref("widget.non-native-theme.scrollbar.style", 2);
+user_pref("widget.non-native-theme.use-theme-accent", true);
 user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
 user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
@@ -20,9 +21,11 @@ user_pref("toolkit.coverage.opt-out", true);
 user_pref("browser.urlbar.trimHttps", true);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.enabled", false);
+user_pref("privacy.userContext.enabled", true);
 user_pref("browser.attribution.enabled", false);
 user_pref("toolkit.coverage.endpoint.base", "");
 user_pref("toolkit.telemetry.server", "data:,");
+user_pref("dom.security.https_only_mode", true);
 user_pref("privacy.userContext.ui.enabled", true);
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("toolkit.telemetry.coverage.opt-out", true);
@@ -43,21 +46,36 @@ user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 
 //*** Performance ***//
 
-user_pref("network.prefetch-next", true);
+user_pref("network.prefetch-next", false);
 user_pref("gfx.canvas.accelerated", true);
-user_pref("network.http.rcwn.enabled", true);
-user_pref("media.cache_readahead_limit", 60);
-user_pref("media.cache_resume_threshold", 30);
+user_pref("network.predictor.enabled", false);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("content.notify.interval", 120000);
+user_pref("network.http.rcwn.enabled", true); 
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("media.cache_resume_threshold", 3600);
 user_pref("layers.gpu-process.enabled", true);
-user_pref("media.memory_cache_max_size", 8192);
+user_pref("media.memory_cache_max_size", 65536);
 user_pref("network.dns.disablePrefetch", true);
+user_pref("network.fetchpriority.enabled", true);
+user_pref("security.tls.enable_0rtt_data", true);
+user_pref("gfx.content.skia-font-cache-size", 20);
+user_pref("network.http.http3.enable_0rtt", true);
+user_pref("gfx.canvas.accelerated.cache-size", 256);
+user_pref("network.websocket.max-connections", 200);
 user_pref("image.mem.decode_bytes_at_a_time", 16384);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+user_pref("network.http.speculative-parallel-limit", 0);
 user_pref("media.hardware-video-decoding.enabled", true);
 user_pref("network.predictor.enable-hover-on-ssl", true);
 user_pref("network.dns.disablePrefetchFromHTTPS", false);
-user_pref("dom.script_loader.bytecode_cache.strategy", 0);
+user_pref("browser.xul.error_pages.expert_bad_cert", false);
 user_pref("media.memory_caches_combined_limit_kb", 524288);
-user_pref("layout.css.grid-template-masonry-value.enabled", false);
+user_pref("network.cookie.sameSite.noneRequiresSecure", true);
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("network.http.max-persistent-connections-per-proxy", 48);
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", false);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
 
 //*** Annoyances ***//
 
@@ -94,7 +112,7 @@ user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
 //*** Miscellaneous ***//
 
 user_pref("media.eme.enabled", true);
-user_pref('image.jxl.enabled', true, locked);
+user_pref("image.jxl.enabled", true, locked);
 user_pref("image.avif.enabled", true, locked);
 user_pref("editor.truncate_user_pastes", true);
 user_pref("browser.urlbar.suggest.calculator", true);
